d065fc188b4ea09030567ade29d63841822d670c
BehruzMulkamonov <126500929+BehruzMulkamonov@users.noreply.github.com>
2023-06-16 18:16:07 +0500

    version2

---
+++ myhome/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone = models.CharField(max_length=13)
+    data = models.DateField()
+    slug = models.SlugField()
+    
+    def __str__(self) -> str:
+        return self.firstname + ' | ' + self.slug
---
+++ myhome/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+from .models import Member
+
+# Create your views here.
+
+def members(request):
+    mymembers = Member.objects.all().values
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers, 
+    }
+    
+    return HttpResponse(template.render(context,request))
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context,request))
